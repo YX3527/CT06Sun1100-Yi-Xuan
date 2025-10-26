ad22d3f933f56098e689cc5dd90fd2ab1e7ae208
YX3527 <lawyxyx@gmail.com>
2025-10-26 04:13:08 +0000

    Oct 26, 2025, 4:13 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,7 +1,7 @@
 let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard" ];
 
 function setup() {
-    createCanvas()
+    createCanvas(200, 300)
 }
 function draw() {
 for (let pokemons = 1; pokemons < 5; pokemons++) {
